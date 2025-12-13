8d767c2a6a984f749eed61c7472cd717d0f36ea3
cs2mosa <mosaashinawy1@gmail.com>
2025-12-14 00:18:09 +0200

    added CLI integration

---
+++ src/cli.py
@@ -0,0 +1,79 @@
+import argparse
+import json
+import sys
+from controllers import XMLController
+from utils import file_io
+
+parser = argparse.ArgumentParser(description="use XML editor in CLI mode")
+commands =parser.add_subparsers(dest='command', help='Available functionalities',required=True)
+
+verify_arg = commands.add_parser('verify', help= 'verify the structure of the XML provided')
+verify_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+verify_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+verify_arg.add_argument('-f', "--fix", required=False, action='store_true', help='fix the XML file...')
+
+format_arg = commands.add_parser('format', help= 'formatting the xml file to the standard format')
+format_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+format_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+
+json_arg = commands.add_parser('json', help= 'transform the XMl file to a json format file')
+json_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+json_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+
+mini_arg = commands.add_parser('mini', help= 'strip spaces in XML file')
+mini_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+mini_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+
+compress_arg = commands.add_parser('compress', help= 'compressing XML file to specified destination')
+compress_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+compress_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+
+decompress_arg = commands.add_parser('decompress', help= 'decompressing XML file to specified destination')
+decompress_arg.add_argument('-i',"--input",required=True,type=str,help='Path to the input XML file')
+decompress_arg.add_argument('-o',"--output",required=False,type=str,help='Path to the output XML file')
+
+
+if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+editor = XMLController()
+args = parser.parse_args()
+if args.command == 'verify':
+    if args.fix and args.output is None:
+        print("invalid usage")
+
+    if file_io.read_file(args.input)[0]:
+        editor.set_xml_string(file_io.read_file(args.input)[1])
+        editor.set_xml_string(editor.validate())
+        ack = editor.format()
+        if args.output is not None:
+            file_io.write_file(args.output, ack)
+        print(ack)
+    else:
+        print("invalid path")
+
+if args.command == 'format':
+    if file_io.read_file(args.input)[0]:
+        editor.set_xml_string(file_io.read_file(args.input)[1])
+        ack = editor.format()
+        if args.output is not None:
+            file_io.write_file(args.output,ack)
+        print(ack)
+    else:
+        print("invalid file path")
+
+if args.command == 'json':
+    ack = file_io.read_file(args.input)
+    if ack[0]:
+        editor.set_xml_string(file_io.read_file(args.input)[1])
+        bool_mes, _, json_data = editor.export_to_json()
+        if bool_mes:
+            if args.output is not None:
+                with open(args.output, 'w', encoding='utf-8') as f:
+                    json.dump(json_data, f, indent=2, ensure_ascii=False)
+            else:
+                print(f"json data format: \n\n{json_data}")
+        else:
+            print("invalid argument")
+
--- src/controllers/xml_controller.py
+++ src/controllers/xml_controller.py
@@ -17,9 +17,8 @@ Date: [Date]
 
 import textwrap
 import re
-import json
-from typing import List, Tuple, Optional
-# from utilities.binary_utils import ByteUtils
+from typing import List, Tuple, Optional, Any
+from ..utils.binary_utils import ByteUtils
 
 
 class XMLController:
@@ -363,7 +362,7 @@ class XMLController:
     # SECTION 5: JSON EXPORT METHOD
     # ===================================================================
 
-    def export_to_json(self, file_path: str) -> Tuple[bool, str, Optional[str]]:
+    def export_to_json(self) -> Tuple[bool, str, Optional[dict[str,Any]] | None]:
         """
         Export XML data to JSON format and save it to a file.
 
@@ -375,7 +374,7 @@ class XMLController:
             tuple: (success: bool, message: str, error: str)
         """
         if self.xml_string is None:
-            return False, "", "No data loaded. Please set a valid XML file first."
+            return False, "No data loaded. Please set a valid XML file first.", None
 
         try:
             tokens = self._get_tokens()
@@ -491,14 +490,10 @@ class XMLController:
             # final check
             final_user_count = len(json_data["users"])
 
-            # handle file I/O
-            with open(file_path, 'w', encoding='utf-8') as f:
-                json.dump(json_data, f, indent=2, ensure_ascii=False)
-
-            return True, f"Successfully exported {final_user_count} users to JSON. File saved: {file_path}", None
+            return True, f"Successfully exported {final_user_count} users to JSON", None
 
         except Exception as e:
-            return False, "", f"Failed to export to JSON using custom parser: {str(e)}"
+            return False, f"Failed to export to JSON using custom parser: {str(e)}" , None
 
     # ===================================================================
     # SECTION 6: Compression and Decompression
